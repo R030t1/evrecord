67e1603e5fbc2a0bbb9874c7cb0e5db1a1e369dd
Sid Spry <github@sid.aeam.us>
2020-12-04 11:35:30 -0600

    Start device code

--- evrecord.cc
+++ evrecord.cc
@@ -1,9 +1,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#include <unistd.h>
+#include <fcntl.h>
+
 #include <string>
-#include <fstream>
 #include <iostream>
+#include <fstream>
+#include <filesystem>
 
 #include <boost/iostreams/filtering_stream.hpp>
 #include <boost/iostreams/filter/gzip.hpp>
@@ -13,8 +17,11 @@
 
 #include "names.h"
 
+namespace fs = std::filesystem;
 using namespace std;
 using namespace boost::iostreams;
+
+void print_device(int fd);
 
 int main(int argc, char *argv[]) {
 	ofstream fs;
@@ -29,5 +36,21 @@ int main(int argc, char *argv[]) {
 	for (int i = 0; keys[i]; i++)
 		printf("%s\n", keys[i]);
 
+	// Gets all of the top level eventN devices.
+	for (const auto &dirent : fs::directory_iterator("/dev/input")) {
+		if (!dirent.is_character_file())	
+			continue;
+		std::cout << dirent << std::endl;
+		int fd = open(dirent.path().c_str(), O_RDWR);
+		print_device(fd);
+		close(fd);
+	}
+
 	return EXIT_SUCCESS;
+}
+
+void print_device(int fd) {
+	char name[256] = "Uknown";
+	ioctl(fd, EVIOCGNAME(sizeof name), name);
+	printf("%s\n", name);
 }
